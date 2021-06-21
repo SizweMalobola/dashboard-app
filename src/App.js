9d000ccee0a86e4cc4d672276b13683a7663e51a
Ngonidzashe Christian Mugumwa <ngoni.mugumwa@gmail.com>
2021-06-21 13:35:59 +0200

    updated firebase.js, security .env

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import firebase from './firebase'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-	const [notes, setNotes] = useState([])
-
-	const ref = firebase.firestore().collection('notes')
-	console.log(ref)
-
 	return (
 		<Router>
 			<div className='App'>
-				<h1 className='pt-4 text-2xl font-semibold bg-blue-dark text-yellow-dark font-ropa-sans'>
-					Hello World
+				<h1 className='pt-4 text-2xl bg-blue-dark text-yellow-dark font-ropa-sans'>
+					Good luck
 				</h1>
-
-				{notes.map(note => (
-					<div key={note.id}>
-						<h2>{note.title}</h2>
-						<p>{note.description}</p>
-					</div>
-				))}
 			</div>
 		</Router>
 	)
--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAjV-ST1e8XMtzUWM87NwpwtTFIoY6adGo",
-   authDomain: "my-dashboard-ad95e.firebaseapp.com",
-   projectId: "my-dashboard-ad95e",
-   storageBucket: "my-dashboard-ad95e.appspot.com",
-   messagingSenderId: "867161555378",
-   appId: "1:867161555378:web:2f707b495eb08c7f1664ab"
- }
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
 
 firebase.initializeApp(firebaseConfig)
- 
+
 export default firebase
