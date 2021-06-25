94193da6413b6d0f4c8cdda9570a5b047fb6bbeb
Ngonidzashe Christian Mugumwa <ngoni.mugumwa@gmail.com>
2021-06-25 18:10:18 +0200

    user auth and firestore database complete for signup page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navigation from '../layout/Navigation'
 
 function Main() {
    return (
-      <div>
-         <h1>Main Page</h1>
+      <div className='min-h-screen px-4 py-2 bg-blue-dark'>
+         <Navigation />
+
+         
       </div>
    )
 }
