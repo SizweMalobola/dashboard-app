799d5fdb3b7d5e577028d3f3e47f9e9e00404903
Ngonidzashe Christian Mugumwa <ngoni.mugumwa@gmail.com>
2021-07-11 16:41:05 +0200

    working on note complete

--- src/components/main/Notes.js
+++ src/components/main/Notes.js
@@ -1,63 +1,75 @@
 import React, { useState, useEffect } from 'react'
 import firebase from '../../firebase'
-import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { v4 as uuidv4 } from 'uuid'
+import cn from 'classnames'
 
 function Notes() {
 	const [todos, setTodos] = useState([])
-	const [input, setInput] = useState('')
-	const db = firebase.firestore().collection('notes')
+	const [note, setNote] = useState('')
+	const [click, setClick] = useState(false)
+	const db = firebase.firestore().collection('todo')
 
-	useEffect(() => {
-		db.onSnapshot(snapshot => {
-			setTodos(
-				snapshot.docs.map(doc => {
-					return {
-						id: doc.id,
-						name: doc.data().todo,
-					}
-				}),
-			)
+	const getTodos = () => {
+		db.onSnapshot(querySnapshot => {
+			const items = []
+			querySnapshot.forEach(doc => {
+				items.push(doc.data())
+			})
+			setTodos(items)
 		})
-	}, [])
-
-	const addTodo = event => {
-		event.preventDefault()
-		db.add({
-			todo: input,
-		})
-		setInput('')
 	}
 
-	const deleteTodo = id => {
-		db.doc(id)
+	useEffect(() => {
+		getTodos()
+	}, [])
+
+	const addTodo = newTodo => {
+		db.doc(newTodo.id)
+			.set(newTodo)
+			.catch(error => {
+				console.log('An error has occurred', error)
+			})
+	}
+
+	const deleteTodo = todo => {
+		db.doc(todo.id)
 			.delete()
-			.then(response => console.log('Deleted', response))
+			.catch(error => {
+				console.log('An error has occurred', error)
+			})
 	}
 
 	return (
 		<div className='flex flex-col items-center h-full p-3'>
-			<form className='flex items-center justify-center w-full'>
+			<div className='flex items-center justify-center w-full mb-3'>
 				<input
 					type='text'
 					name='task'
 					id='task'
-					onChange={event => setInput(event.target.value)}
-					className='w-[80%] px-4 py-1 mx-2 rounded-full outline-none bg-blue-dark text-teal-light transition'
+					value={note}
+					onChange={event => setNote(event.target.value)}
+					className='w-[80%] px-4 py-1 md:py-2 mx-2 rounded-full outline-none bg-blue-dark text-teal-light transition'
 				/>
 
 				<button
 					type='submit'
-					onClick={addTodo}
-					className='flex items-center justify-center w-8 h-8 text-xl text-white transition rounded-full focus:outline-none bg-blue-dark font-poppins hover:text-yellow-dark active:text-white focus:text-yellow-dark hover:animate-spin'
+					onClick={() => addTodo({ note, id: uuidv4() })}
+					className='flex items-center justify-center w-8 h-8 text-xl text-white transition rounded-full xl:w-10 xl:h-10 focus:outline-none bg-blue-dark font-poppins hover:text-yellow-dark active:text-white focus:text-yellow-dark hover:animate-spin'
 				>
 					+
 				</button>
-			</form>
+			</div>
 			<div className='relative flex flex-col items-center justify-center w-full h-auto overflow-scroll-y'>
 				{todos.map(todo => (
-					<div className='relative z-10 w-full md:h-10 px-4 py-1 mx-2 my-3 rounded-full outline-none xl:max-w-64 bg-blue-dark text-gray-300 xl:w-[90%] transition flex items-center justify-between'>
+					<div
+						key={todo.id}
+						className='relative z-10 w-full px-3 py-1 md:py-2 mx-2 my-1 rounded-full outline-none xl:max-w-64 bg-blue-dark text-gray-300 xl:w-[90%] transition flex items-center justify-between'
+					>
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
+							onClick={() => {
+								setClick(is => !is)
+							}}
 							className='w-6 h-6 transition cursor-pointer text-blue-light hover:text-green-500 focus:text-green-500 hover:animate-pulse'
 							fill='none'
 							viewBox='0 0 24 24'
@@ -70,12 +82,17 @@ function Notes() {
 								d='M5 13l4 4L19 7'
 							/>
 						</svg>
-						<p className='text-xs text-left w-[80%] md:text-base'>
-							{input.value}
+						<p
+							className={cn('text-xs text-left w-[90%] md:text-base', {
+								'line-through': click,
+								transition: click,
+							})}
+						>
+							{todo.note}
 						</p>
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
-							onClick={() => deleteTodo(todo.id)}
+							onClick={() => deleteTodo(todo)}
 							className='w-6 h-6 transition transform cursor-pointer text-blue-light hover:text-red-500 hover:animate-bounce focus:text-red-500'
 							fill='none'
 							viewBox='0 0 24 24'
